f7fbb4f761b22ca018204abb3523174b8d798edd
akmalist <akmalist@gmail.com>
2019-09-24 13:31:20 -0700

    add scroll up button

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
+ //Get the button
+const mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
 function pictureSwitch(myImage){
- myImage.src="images/robot.svg";
+ myImage.src="/images/robot.svg";
  
 };
 
 function mouseAway(myImage){
-  myImage.src="images/robot2.svg";
+  myImage.src="/images/robot2.svg";
 };
+
+ 
+
+
