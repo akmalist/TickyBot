8d712123762dcf551e346c649dd5403d6f0b3468
Aki John <akmal@Akis-MacBook-Pro.local>
2019-09-24 22:01:08 -0700

    final version

--- script.js
+++ script.js
@@ -1,33 +1,24 @@
+
  //Get the button
 const mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 100px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = () => scrollFunction();
 
-function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+// When the user scrolls down 300px from the top of the document, show the button
+
+const scrollFunction = () => {
+  document.body.scrollTop > 300 || document.documentElement.scrollTop > 300 ?  mybutton.style.display = "block" : mybutton.style.display = "none";
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+const topFunction = () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
 
+const pictureSwitch = (myImage) =>  myImage.src="/images/robot.svg";
+const mouseAway = (myImage) => myImage.src="/images/robot2.svg";
 
-function pictureSwitch(myImage){
- myImage.src="/images/robot.svg";
- 
-};
 
-function mouseAway(myImage){
-  myImage.src="/images/robot2.svg";
-};
-
- 
 
 
